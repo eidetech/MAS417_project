cadbc5c1f50b36e1a55908b95c4122b13ea509fb
eidetech <marcuseide@me.com>
2022-11-10 12:12:01 +0100

    Before swapping xx,xy,yx,yy

--- stl_generator/stl_generator.py
+++ stl_generator/stl_generator.py
@@ -28,6 +28,7 @@ class StlGenerator:
         self.xy_vertices_top = [0] * self.height * self.width
         self.yy_vertices_top = [0] * self.height * self.width
         self.yx_vertices_top = [0] * self.height * self.width
+        self.bottom_data = self.height_data * 0 - self.thickness
 
         # Top vertices (which is the actual height data points)
         idx = 0
@@ -37,6 +38,7 @@ class StlGenerator:
                 self.grid_2d[idx] = [x, y]
                 idx+=1
 
+
         # Bottom vertices (which is just a flat plane the size of height_data array)
         idx = 0
         for y, row in enumerate(self.height_data):
@@ -44,16 +46,20 @@ class StlGenerator:
                 self.bottom_vertices[idx] = [x,y, -self.thickness] # Create an array containing [x,y,z] coordinate for the bottom vertices
                 idx+=1
 
-        # yy vertices
-        self.yy_vertices_top = self.height_data[self.width - 1, :self.height]
+        # Side vertices
+        idx = 0
+        for y, row in enumerate(self.height_data):
+            for x, column in enumerate(row):
+                self.xx_vertices_top[idx] = [0,x, self.height_data[self.width-1, x]]
+                self.yx_vertices_top[idx] = [499, x, self.height_data[0, x]]
+                idx+=1
 
-        # yx vertices
-        self.yx_vertices_top = self.height_data[:self.width, self.height - 1]
-        # xx vertices
-        self.xx_vertices_top = self.height_data[:self.width, 0]
-        self.xx_vertices_top = self.xx_vertices_top[::-1] # TODO: Not sure why I have to reverse the order of this list, could probably select indexes in a smarter way
+        # yy vertices
+        self.yy_vertices_top = self.top_vertices[:self.width]
+        #self.yy_vertices_bottom = self.top_vertices[:self.width]
+
         # xy vertices
-        self.xy_vertices_top = self.height_data[0, :self.height]
+        self.xy_vertices_top = self.top_vertices[-self.width:]
 
         if self.graph:
             xxplot = pv.Chart2D()
@@ -111,8 +117,7 @@ class StlGenerator:
         self.bottom_mesh.save('bottom_mesh.stl')
 
     def create_side_meshes(self):
-        # TODO: implement function
-        print("Will create l8er.")
+        print(self.yx_vertices_top)
 
     def combine_meshes(self):
         """
