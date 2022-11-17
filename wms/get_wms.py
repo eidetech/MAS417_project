5c3a9473c3d6001a9bee78d06a5143832ac90901
eidetech <marcuseide@me.com>
2022-11-17 18:55:44 +0100

    Updated menu

--- wms/get_wms.py
+++ wms/get_wms.py
@@ -2,6 +2,9 @@ from io import BytesIO
 from PIL import Image, ImageFilter
 import requests
 import numpy as np
+from colorama import init, AnsiToWin32, Fore, Style
+import sys
+stream = AnsiToWin32(sys.stderr).stream
 
 class GetWMS:
     def __init__(self, debug):
@@ -22,21 +25,19 @@ class GetWMS:
         user_input is used for retrieving input data from user to the program
         """
         global bbox2string, input_list, width, height
-
-        print("----------MAP TO 3D-PRINT-MODEL----------")
-        print(
-            "This program converts a terrain model of a chosen \nNorwegian geographic area to a .stl_generator file for 3d printing\n")
-        print(
-            "1. First go to Google Maps and find the center point of \nan area in Norway you want to print a height model of.")
-        print("2. Then enter the coordinates, the size of area in meters,\na scaling factor, the thickness of the 3d-print and a filname.")
-        print(
-            "Examples: \nGaustadtoppen[ 59.854102, 8.648146, 2000, 1, 10, gaustadtoppen.stl ] \nGeiranger[ 62.119509, 7.148389, 15000, 0.5, 10, geiranger.stl ]\n")
+        print(Fore.LIGHTBLUE_EX, file=stream)
+        print("######################## MAP TO 3D-PRINT-MODEL ########################")
+        print("#   This program converts a terrain model of a chosen Norwegian       #\n#   geographic area to a .stl file for 3d printing.                   #")
+        print("#   1. First go to Google Maps and find the center point of an area   #\n#   in Norway you want to print a height model of.                    #")
+        print("#   2. Then enter the coordinates, the size of area in meters,        #\n#   a scaling factor, the thickness of the 3d-print and a filname.    #")
+        print("#######################################################################")
+        print(Fore.LIGHTYELLOW_EX, file=stream)
+        print("Examples: \nGaustadtoppen[ 59.854102, 8.648146, 2000, 1, 10, gaustadtoppen.stl ] \nGeiranger[ 62.119509, 7.148389, 15000, 0.5, 10, geiranger.stl ]\n")
 
         n = 6  # number of input_list elements
-
-        input_list = list(
-            input("Enter the following values, separated by comma: lat, lon, size, scale factor, print thickness, filename.stl: ").strip().split(','))[:n]
-
+        print(Fore.LIGHTBLUE_EX, file=stream)
+        input_list = list(input("Enter the following values, separated by comma: lat, lon, size, scale factor, print thickness, filename.stl: ").strip().split(','))[:n]
+        print(Style.RESET_ALL, file=stream)
         deg2meter_list = [40000 * 2, 90000 * 2]
         # Convert the 7 first input entries to float numbers
         for i in range(5):
@@ -48,35 +49,8 @@ class GetWMS:
                 input_list[0] + (input_list[2] / deg2meter_list[1])]
         bbox2string = ','.join(str(i) for i in bbox)
 
-        resolution = 500  # resolution of picture width in pixels
-        height = 500#int(resolution / (input_list[2] / input_list[2]))
-        width = resolution
-        print(height)
-
-    def dev_input(self):
-        """
-        dev_input is used for inputting necessary data to the program which will later be provided by user
-        """
-        global bbox2string, input_list, width, height
-        # Static Geiranger for dev
-        input_list = [0] * 8  # 6x1 array of 0's
-        input_list[0] = 62.119509  # Lat
-        input_list[1] = 7.148309  # Lon
-        input_list[2] = 15000  # Width
-        input_list[3] = 15000  # Height
-        input_list[4] = 500  # Res
-        input_list[5] = 0.5  # Scale/himalaya
-        input_list[6] = 10  # Thickness of the stl (height under the surface)
-        input_list[7] = "geiranger.stl" # stl file name
-        bbox = [input_list[1] - (input_list[2] / 50000), input_list[0] - (input_list[3] / 50000),
-                input_list[1] + (input_list[2] / 50000), input_list[0] + (input_list[3] / 50000)]
-        bbox2string = ','.join(str(i) for i in bbox)
-
-        resolution = int(input_list[4])  # resolution of picture width in pixels
-        height = int(resolution / (input_list[2] / input_list[3]))
-        width = resolution
-
-
+        height = 500
+        width = 500
 
     def __get_api_data(self):
         """
